970b6d577287edcae665a5b9b0fa9a6e9a726369
Andracarys <49758969+Andracarys@users.noreply.github.com>
2025-11-03 16:45:01 -0300

    drive update

--- CS50/cs50_7.py
+++ CS50/cs50_7.py
@@ -305,3 +305,14 @@ def main():
 
 
 main()
+
+# Problem 7 Homework
+# NUMB3RS
+
+# implement a function called validate that expects an IPv4 address as input as a str
+# and then returns True or False, respectively, if that input is a valid IPv4 address or not.
+
+# An IPv4 address is a numeric identifier that a device (or, on TV, hacker)
+# uses to communicate on the internet, akin to a postal address in the real world,
+# typically formatted in dot-decimal notation as #.#.#.#
+# But each # should be a number between 0 and 255, inclusive.
