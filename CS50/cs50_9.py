1685f12fbe9cc28241ae3b3577e96f3d39ebef02
Andrea Duarte <49758969+Andracarys@users.noreply.github.com>
2025-12-17 18:54:33 +0000

    added lecture 10

---
+++ CS50/cs50_9.py
@@ -0,0 +1,2 @@
+# CS50P - Lecture 9 - Et Cetera
+
---
+++ CS50/playground.py
@@ -0,0 +1,10 @@
+from datetime import date
+
+
+def main():
+    today = date.today()
+    print(f"Today's date is: {today}")
+
+
+if __name__ == "__main__":
+    main()
