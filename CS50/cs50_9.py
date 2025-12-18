dfdae1ab81bfe60782b4e1b633cd3aaf4401fffe
Andracarys <49758969+Andracarys@users.noreply.github.com>
2025-12-17 22:46:51 -0300

    content update

--- CS50/cs50_9.py
+++ CS50/cs50_9.py
@@ -1,2 +1,216 @@
 # CS50P - Lecture 9 - Et Cetera
 
+# Filters out duplicate houses using loop
+
+students = [
+    {"name": "Hermione", "house": "Gryffindor"},
+    {"name": "Harry", "house": "Gryffindor"},
+    {"name": "Ron", "house": "Gryffindor"},
+    {"name": "Draco", "house": "Slytherin"},
+    {"name": "Padma", "house": "Ravenclaw"},
+]
+
+houses = []
+for student in students:
+    if student["house"] not in houses:
+        houses.append(student["house"])
+
+for house in sorted(houses):
+    print(house)
+
+
+# Filters out duplicate houses using set
+
+students = [
+    {"name": "Hermione", "house": "Gryffindor"},
+    {"name": "Harry", "house": "Gryffindor"},
+    {"name": "Ron", "house": "Gryffindor"},
+    {"name": "Draco", "house": "Slytherin"},
+    {"name": "Padma", "house": "Ravenclaw"},
+]
+
+houses = set()
+for student in students:
+    houses.add(student["house"])
+
+for house in sorted(houses):
+    print(house)
+
+
+# Uses global
+
+balance = 0
+
+
+def main():
+    print("Balance:", balance)
+    deposit(100)
+    withdraw(50)
+    print("Balance:", balance)
+
+
+def deposit(n):
+    global balance
+    balance += n
+
+
+def withdraw(n):
+    global balance
+    balance -= n
+
+
+if __name__ == "__main__":
+    main()
+
+
+# Uses class
+
+
+class Account:
+    def __init__(self):
+        self._balance = 0
+
+    @property
+    def balance(self):
+        return self._balance
+
+    def deposit(self, n):
+        self._balance += n
+
+    def withdraw(self, n):
+        self._balance -= n
+
+
+def main():
+    account = Account()
+    print("Balance:", account.balance)
+    account.deposit(100)
+    account.withdraw(50)
+    print("Balance:", account.balance)
+
+
+if __name__ == "__main__":
+    main()
+
+
+# Demonstrates a constant
+
+MEOWS = 3 # if capitalized, the constant variable should not be messed with
+
+for _ in range(MEOWS):
+    print("meow")
+
+
+# Demonstrates a class constant
+
+
+class Cat:
+    MEOWS = 3
+
+    def meow(self):
+        for _ in range(Cat.MEOWS):
+            print("meow")
+
+
+cat = Cat()
+cat.meow()
+
+
+# Success
+
+
+def meow(n: int):
+    for _ in range(n):
+        print("meow")
+
+
+number: int = int(input("Number: "))
+meow(number)
+
+
+# Prints None because mistakes meow as having a return value
+
+
+def meow(n: int):
+    for _ in range(n):
+        print("meow")
+
+
+number: int = int(input("Number: "))
+meows: str = meow(number) 
+print(meows) # prints none at the bottom because the function meow has no return
+
+
+# Annotates return value, ... does not return a value
+
+
+def meow(n: int) -> None:
+    for _ in range(n):
+        print("meow")
+
+
+number: int = int(input("Number: "))
+meows: str = meow(number)
+print(meows)
+
+
+# Success
+
+
+def meow(n: int) -> str:
+    return "meow\n" * n
+
+
+number: int = int(input("Number: "))
+meows: str = meow(number)
+print(meows, end="")
+
+
+# Adds docstring to function.
+
+
+def meow(n):
+    """Meow n times."""
+    return "meow\n" * n
+
+
+number = int(input("Number: "))
+meows = meow(number)
+print(meows, end="")
+
+
+# Uses Sphinx docstring format
+
+
+def meow(n):
+    """
+    Meow n times.
+
+    :param n: Number of times to meow
+    :type n: int
+    :raise TypeError: If n is not an int
+    :return: A string of n meows, one per line
+    :rtype: str
+    """
+    return "meow\n" * n
+
+
+number = int(input("Number: "))
+meows = meow(number)
+print(meows, end="")
+
+
+# Uses command-line argument
+
+# import sys
+
+if len(sys.argv) == 1:
+    print("meow")
+elif len(sys.argv) == 3 and sys.argv[1] == "-n":
+    n = int(sys.argv[2])
+    for _ in range(n):
+        print("meow")
+else:
+    print("usage: meows11.py [-n NUMBER]")
+
+
